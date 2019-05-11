036b5f82150a81a9bed8a7c0684330ccc7a8e7e0
klaudia <klaudia.styrcz@gmail.com>
2019-05-11 20:19:44 +0200

    adding carousel news

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#news-slider").owlCarousel({
+        items:4,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,2],
+        itemsMobile:[600,1],
+        pagination:false,
+        navigationText:false,
+        autoPlay:true,
+    })
+
+});
